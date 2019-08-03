19fa18627f24cdefd658bf32b1b51430f3f43f92
HamoHamo5 <2533797575@qq.com>
2019-08-03 20:56:18 +0800

    Updates

---
+++ precache-manifest.9d242559e94ed80698daace9ba1640ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ed70cd755fa899d54cb8636d4ac66e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "079c8780bef606031b79",
+    "url": "/monsters-rolodex/static/css/main.66346678.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "079c8780bef606031b79",
+    "url": "/monsters-rolodex/static/js/main.75cb252a.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
